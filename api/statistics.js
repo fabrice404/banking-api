df047aab5ebad5de262b897fefe5c22b374f238f
fabrice404 <fabrice.lamant@gmail.com>
2018-01-09 18:26:19 +0100

    Add statistics

---
+++ api/statistics.js
@@ -0,0 +1,33 @@
+const tools = require('./tools')
+
+module.exports.operations_dailyBalance = (event, context, callback) => {
+  let accountId = event.queryStringParameters.a
+  let dateStart = new Date(event.queryStringParameters.y, event.queryStringParameters.m - 1, 1)
+  let dateEnd = new Date(event.queryStringParameters.y, event.queryStringParameters.m - 1, 1)
+  dateEnd.setMonth(dateEnd.getMonth() + 1)
+  dateEnd.setDate(dateEnd.getDate() - 1)
+
+  let knex = tools.initKnex()
+  knex.select()
+    .from(knex.raw('statistics_operations_dailyBalance(?,?,?)', [accountId, dateStart, dateEnd]))
+    .then(stats => {
+      const response = tools.buildResponse(JSON.stringify(stats))
+      callback(null, response)
+      return knex.destroy()
+    })
+}
+
+module.exports.account_dayBalance = (event, context, callback) => {
+  let accountId = event.queryStringParameters.a
+  let date = new Date(event.queryStringParameters.y, event.queryStringParameters.m - 1, 1)
+
+  let knex = tools.initKnex()
+  knex.select()
+    .from(knex.raw('statistics_account_dayBalance(?,?)', [accountId, date]))
+    .then(stats => {
+      let stat = stats[0]
+      const response = tools.buildResponse(JSON.stringify(stat))
+      callback(null, response)
+      return knex.destroy()
+    })
+}
